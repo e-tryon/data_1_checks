498973480ec0a472a746e0a15a4a00ff0eec2cd2
e-tryon <112709749+e-tryon@users.noreply.github.com>
2022-09-19 08:26:45 -0400

    Add files via upload

---
+++ knowledge_check_one.py
@@ -0,0 +1,25 @@
+# prints the words Hello World
+print('Hello World')
+
+# list of football teams
+some_nfl_teams = ["Bengals" , "Broncos" , "Bucs" , "Rams"]
+
+# print the second value in the list
+print(some_nfl_teams[1])
+
+# a dictionary with 2 keys and 2 values
+football_dict = {"Bengals" : "Orange" , "Raiders"  : 'Black'}
+
+# print one of the values of that dictionary
+print(football_dict["Bengals"])
+
+# a tuple with 4 values
+some_college_teams = ("Gators", "Wildcats", "Bulldogs", "Volunteers")
+
+# print the third value in the tuple
+print(some_college_teams[2])
+
+
+
+
+
